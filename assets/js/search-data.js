0245d3bb6d75b55e027be0edecb41a27cd01a5a2
mrs-metu <mrs.lab.metu@gmail.com>
2025-04-19 11:33:36 +0000

    Deploying to gh-pages from @ mrs-metu/mrs-metu.github.io@e86d5e38f30225812a9a04c10c17e34066677fd6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -77,30 +77,4 @@ ninja.data = [{
           description: "develop automated ultrasound imaging systems",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
-    },];
+            },},];
